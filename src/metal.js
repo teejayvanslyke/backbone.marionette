c1f3e05af2944f75f1ea99122a167cfa1a16f542
paulfalgout <paul@otterball.com>
2015-05-12 00:29:08 -0500

    Metal testing WIP

--- src/metal.js
+++ src/metal.js
@@ -1,3 +1,51 @@
-// Add Backbone.Metal extensions here
-// 1. Marionette.Class
-// 2. Marionette.Error and Marionette.Deprecate
+Marionette.Class = Metal.Class.extend({
+  constructor: function(options) {
+    this.options = _.extend({}, _.result(this, 'options'), options);
+
+    this._super.apply(this, arguments);
+  },
+
+  destroy: function() {
+    this.triggerMethod('before:destroy');
+    this.triggerMethod('destroy');
+    this.stopListening();
+
+    return this;
+  },
+
+  // Import the `triggerMethod` to trigger events with corresponding
+  // methods if the method exists
+  triggerMethod: Marionette.triggerMethod,
+
+  // A handy way to merge options onto the instance
+  mergeOptions: Marionette.mergeOptions,
+
+  // Proxy `getOption` to enable getting options from this or this.options by name.
+  getOption: Marionette.proxyGetOption,
+
+  // Proxy `bindEntityEvents` to enable binding view's events from another entity.
+  bindEntityEvents: Marionette.proxyBindEntityEvents,
+
+  // Proxy `unbindEntityEvents` to enable unbinding view's events from another entity.
+  unbindEntityEvents: Marionette.proxyUnbindEntityEvents
+});
+
+var initKeys = ['constructor', 'initialize'];
+var eventKeys = _.keys(Backbone.Events);
+var classKeys = _.keys(Marionette.Class);
+
+function classify(obj) {
+  var protoProps = _.extend({constructor: obj}, _.omit(obj.prototype, eventKeys, initKeys));
+  var staticProps = _.omit(obj, classKeys, initKeys);
+
+  return Marionette.Class.extend(protoProps, staticProps);
+}
+
+Marionette.Mixin      = Metal.Mixin;
+Marionette.Events     = Backbone.Events     = new Marionette.Mixin(Backbone.Events);
+Marionette.Model      = Backbone.Model      = classify(Backbone.Model);
+Marionette.Collection = Backbone.Collection = classify(Backbone.Collection);
+Backbone.View         = classify(Backbone.View);
+Marionette.Router     = Backbone.Router     = classify(Backbone.Router);
+Marionette.History    = Backbone.History    = classify(Backbone.History);
+Marionette.history    = Backbone.history    = new Marionette.History();
